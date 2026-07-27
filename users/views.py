0359f60ddfc2ed294255fbc0aec556332bf5ffac
Andrey Bilenko <olimp_72@ukr.net>
2026-07-27 23:28:23 +0300

    feat(users): add views for registration and managing user profile

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from users.serializers import UserSerializer
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
